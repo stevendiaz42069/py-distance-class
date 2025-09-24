40b688bc1419f46d29cfdf91579e7f197600c87b
steven42069 <stevendiaz152@gmail.com>
2025-09-24 18:10:55 -0400

    boogie oogie

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 - Read [the guideline](https://github.com/mate-academy/py-task-guideline/blob/main/README.md) before start
 - Follow [Python magic methods](https://rszalski.github.io/magicmethods/) if you stuck
 
-Every day you have morning running. You want to store your result. 
+Every day you have morning running. You want to store your result.
 For easier calculating, comparison and printing your result write class
 `Distance`.
 
@@ -12,6 +12,7 @@ one argument `km` and saves it to `self.km`.
 
 For `Distance` class you should implement such magic
 methods:
+
 ```python
 __init__
 distance = Distance(20)  # distance.km == 20
@@ -20,14 +21,14 @@ __str__
 distance = Distance(20)
 print(distance)  # "Distance: 20 kilometers."
 
-__repr__
+
 distance = Distance(20)
 repr(distance)  # "Distance(km=20)"
 
 __add__
 distance1 = Distance(20)
 distance2 = Distance(30)
-distance3 = distance1 + distance2  
+distance3 = di__repr__stance1 + distance2
 
 # isinstance(distance3, Distance) is True
 # distance3.km == 50
@@ -49,14 +50,14 @@ distance += 30  # distance.km == 50
 
 __mul__
 distance1 = Distance(20)
-distance2 = distance1 * 5  
+distance2 = distance1 * 5
 
 # isinstance(distance2, Distance) is True
 # distance2.km == 100
 
 __truediv__
 distance1 = Distance(20)
-distance2 = distance1 / 7  
+distance2 = distance1 / 7
 
 # isinstance(distance2, Distance) is True
 # distance2.km == 2.85
@@ -76,5 +77,7 @@ distance == 100  # False
 distance <= 49  # False
 distance >= 50  # True
 ```
-___
+
+---
+
 ### Note: Check your code using this [checklist](checklist.md) before pushing your solution.
--- app/main.py
+++ app/main.py
@@ -1,3 +1,58 @@
 class Distance:
-    # Write your code here
-    pass
+
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(
+        self,
+    ) -> str:
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: type(None)) -> int:
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        elif isinstance(other, (int, float)):
+            return Distance(self.km + other)
+        return NotImplemented
+
+    def __iadd__(self, other: type(None)) -> int:
+        if isinstance(other, Distance):
+            self.km += other.km
+            return self
+        elif isinstance(other, (int, float)):
+            self.km += other
+            return self
+        return NotImplemented
+
+    def __mul__(self, other: type(None)) -> int:
+        if isinstance(other, (int, float)):
+            return Distance(self.km * other)
+
+    def __truediv__(self, other: type(None)) -> int:
+        if isinstance(other, (int, float)):
+            return Distance(round(self.km / other, 2))
+
+    def _compare(self, other: type(None), method: type(None)) -> bool:
+        if isinstance(other, Distance):
+            return method(self.km, other.km)
+        elif isinstance(other, (int, float)):
+            return method(self.km, other)
+        return NotImplemented
+
+    def __eq__(self, other: type(None)) -> int:
+        return self._compare(other, lambda a, b: a == b)
+
+    def __lt__(self, other: type(None)) -> int:
+        return self._compare(other, lambda a, b: a < b)
+
+    def __le__(self, other: type(None)) -> int:
+        return self._compare(other, lambda a, b: a <= b)
+
+    def __gt__(self, other: type(None)) -> int:
+        return self._compare(other, lambda a, b: a > b)
+
+    def __ge__(self, other: type(None)) -> int:
+        return self._compare(other, lambda a, b: a >= b)

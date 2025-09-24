545eb7c59cd89a02e1fadf86f0b3a515439934b9
stevendiaz42069 <141751682+stevendiaz42069@users.noreply.github.com>
2025-09-24 22:44:45 +0000

    annotation changes

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
 
     def __init__(self, km: int) -> None:
@@ -11,14 +14,14 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: type(None)) -> int:
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
         return NotImplemented
 
-    def __iadd__(self, other: type(None)) -> int:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
@@ -27,32 +30,34 @@ class Distance:
             return self
         return NotImplemented
 
-    def __mul__(self, other: type(None)) -> int:
+    def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
 
-    def __truediv__(self, other: type(None)) -> int:
+    def __truediv__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
 
-    def _compare(self, other: type(None), method: type(None)) -> bool:
+    def _compare(
+        self, other: Distance | int | float, method: Callable[float, float]
+    ) -> bool:
         if isinstance(other, Distance):
             return method(self.km, other.km)
         elif isinstance(other, (int, float)):
             return method(self.km, other)
         return NotImplemented
 
-    def __eq__(self, other: type(None)) -> int:
+    def __eq__(self, other: Distance | int | float) -> bool:
         return self._compare(other, lambda a, b: a == b)
 
-    def __lt__(self, other: type(None)) -> int:
+    def __lt__(self, other: Distance | int | float) -> bool:
         return self._compare(other, lambda a, b: a < b)
 
-    def __le__(self, other: type(None)) -> int:
+    def __le__(self, other: Distance | int | float) -> bool:
         return self._compare(other, lambda a, b: a <= b)
 
-    def __gt__(self, other: type(None)) -> int:
+    def __gt__(self, other: Distance | int | float) -> bool:
         return self._compare(other, lambda a, b: a > b)
 
-    def __ge__(self, other: type(None)) -> int:
+    def __ge__(self, other: Distance | int | float) -> bool:
         return self._compare(other, lambda a, b: a >= b)
